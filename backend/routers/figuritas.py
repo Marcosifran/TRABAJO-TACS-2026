27439e1d80f22a45b9c348a6912ad47cdc9a35b7
Marcos <ifranmarcos5@gmail.com>
2026-04-09 16:22:30 -0300

    Front: Dashboard con UI y funcionalidad de eliminar

--- backend/routers/figuritas.py
+++ backend/routers/figuritas.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from database import db_figuritas
 from schemas import Figurita
 
 router = APIRouter(prefix="/figuritas", tags=["Figuritas"])
+
+
 
 @router.post("/")
 def publicar_figuritas(figu: Figurita):
@@ -14,3 +16,11 @@ def publicar_figuritas(figu: Figurita):
 @router.get("/")
 def obtener_figuritas():
     return {"disponibles": db_figuritas}
+
+@router.delete("/{figurita_id}")
+async def eliminar_figurita(figurita_id: int):
+    for index, figu in enumerate(db_figuritas):
+        if figu["id"] == figurita_id:
+            db_figuritas.pop(index)
+            return {"message": "Figurita eliminada"}
+    raise HTTPException(status_code=404, detail="Figurita no encontrada")
