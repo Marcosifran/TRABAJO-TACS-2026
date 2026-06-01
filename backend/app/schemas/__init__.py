5428851e897bc80bbf4a638241f7ee704a970c8f
elioflo <eliofloreschoque2357@gmail.com>
2026-06-01 09:14:40 -0300

    update ci/ruff errors

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,16 +1,59 @@
-from app.schemas.album import FiguritaAlbumCreate, FiguritaAlbumResponse, FiguritaAlbumUpdate
-from app.schemas.calificacion import CalificacionCreate, CalificacionResponse, ReputacionResponse
-from app.schemas.faltante import FaltanteCreate, FaltanteResponse, FaltanteUpdate
-from app.schemas.figurita import FiguritaCreate, FiguritaResponse, FiguritaUpdate
-from app.schemas.intercambio import (
-    EstadoIntercambio,
-    EstadoRespuestaIntercambio,
-    IntercambioCreate,
-    IntercambioDecision,
-    IntercambioResponse,
-)
-from app.schemas.maestro import JugadorMaestroResponse
-from app.schemas.oferta import OfertaCreate, OfertaDecision
-from app.schemas.publicacion import PublicacionCreate, PublicacionResponse, SugerenciaResponse, TipoIntercambio
-from app.schemas.subasta import EstadoSubasta, SubastaCreate, SubastaResponse
-from app.schemas.usuario import UsuarioResponse
+from app.schemas.album import FiguritaAlbumCreate as FiguritaAlbumCreate
+from app.schemas.album import FiguritaAlbumResponse as FiguritaAlbumResponse
+from app.schemas.album import FiguritaAlbumUpdate as FiguritaAlbumUpdate
+from app.schemas.calificacion import CalificacionCreate as CalificacionCreate
+from app.schemas.calificacion import CalificacionResponse as CalificacionResponse
+from app.schemas.calificacion import ReputacionResponse as ReputacionResponse
+from app.schemas.faltante import FaltanteCreate as FaltanteCreate
+from app.schemas.faltante import FaltanteResponse as FaltanteResponse
+from app.schemas.faltante import FaltanteUpdate as FaltanteUpdate
+from app.schemas.figurita import FiguritaCreate as FiguritaCreate
+from app.schemas.figurita import FiguritaResponse as FiguritaResponse
+from app.schemas.figurita import FiguritaUpdate as FiguritaUpdate
+from app.schemas.intercambio import EstadoIntercambio as EstadoIntercambio
+from app.schemas.intercambio import EstadoRespuestaIntercambio as EstadoRespuestaIntercambio
+from app.schemas.intercambio import IntercambioCreate as IntercambioCreate
+from app.schemas.intercambio import IntercambioDecision as IntercambioDecision
+from app.schemas.intercambio import IntercambioResponse as IntercambioResponse
+from app.schemas.maestro import JugadorMaestroResponse as JugadorMaestroResponse
+from app.schemas.oferta import OfertaCreate as OfertaCreate
+from app.schemas.oferta import OfertaDecision as OfertaDecision
+from app.schemas.publicacion import PublicacionCreate as PublicacionCreate
+from app.schemas.publicacion import PublicacionResponse as PublicacionResponse
+from app.schemas.publicacion import SugerenciaResponse as SugerenciaResponse
+from app.schemas.publicacion import TipoIntercambio as TipoIntercambio
+from app.schemas.subasta import EstadoSubasta as EstadoSubasta
+from app.schemas.subasta import SubastaCreate as SubastaCreate
+from app.schemas.subasta import SubastaResponse as SubastaResponse
+from app.schemas.usuario import UsuarioResponse as UsuarioResponse
+
+__all__ = [
+    "CalificacionCreate",
+    "CalificacionResponse",
+    "EstadoIntercambio",
+    "EstadoRespuestaIntercambio",
+    "EstadoSubasta",
+    "FaltanteCreate",
+    "FaltanteResponse",
+    "FaltanteUpdate",
+    "FiguritaAlbumCreate",
+    "FiguritaAlbumResponse",
+    "FiguritaAlbumUpdate",
+    "FiguritaCreate",
+    "FiguritaResponse",
+    "FiguritaUpdate",
+    "IntercambioCreate",
+    "IntercambioDecision",
+    "IntercambioResponse",
+    "JugadorMaestroResponse",
+    "OfertaCreate",
+    "OfertaDecision",
+    "PublicacionCreate",
+    "PublicacionResponse",
+    "ReputacionResponse",
+    "SubastaCreate",
+    "SubastaResponse",
+    "SugerenciaResponse",
+    "TipoIntercambio",
+    "UsuarioResponse",
+]
